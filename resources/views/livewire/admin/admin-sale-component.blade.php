c5ff654429dc57554683dd3406db9f5408275ccd
eKarimoff <elyorbekkarimov3@gamil.com>
2022-01-24 16:09:55 +0500

    master

--- resources/views/livewire/admin/admin-sale-component.blade.php
+++ resources/views/livewire/admin/admin-sale-component.blade.php
@@ -4,7 +4,7 @@
           <div class="col-md-12">
               <div class="panel panel-default">
                   <div class="panel-heading">
-                      Sale Settings
+                      Sale Setting
                   </div>
                 
                   <div class="panel-body">
